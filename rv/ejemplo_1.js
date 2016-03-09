4d356faf9c509d7b6028d8446333632a6d76c99e
FerrerPonce <dianaferrerponce@gmail.com>
2016-03-09 11:24:31 -0600

    Update ejemplo_1.js

--- rv/ejemplo_1.js
+++ rv/ejemplo_1.js
@@ -1,6 +1,6 @@
 function setup(){
 var forma= new THREE.BoxGeometry(1,1,1);
-var material= new THREE.MeshBasicMaterial({color: 0x00f00});
+var material= new THREE.MeshBasicMaterial({color: 0x00ff00});
 malla= new THREE.Mesh(forma, material);
 
 escena= new THREE.Scene();
