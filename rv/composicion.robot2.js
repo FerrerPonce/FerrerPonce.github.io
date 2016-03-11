cb183a3ec967a3c5df3e7fdb1fb5c3d3a10fcb5f
FerrerPonce <dianaferrerponce@gmail.com>
2016-03-10 19:37:58 -0600

    Update composicion.robot2.js

--- rv/composicion.robot2.js
+++ rv/composicion.robot2.js
@@ -1,5 +1,6 @@
 function Robotina(){
 //se plantea las geometrias a utillixzar
+THREE.Object3D.call(this);//utilizamos las propiedades de Object3D
 
 var esferaForma= new THREE.SphereGeometry(0.5);
 var esferaForma2= new THREE.SphereGeometry(1.25);
@@ -10,8 +11,33 @@ var cilindroForma4= new THREE.CylinderGeometry(0.3,0.3,2);//cuello
 var material= new THREE.MeshNormalMaterial();
  //Para generar una forma combinada se requiere de las mallas poaras poder desplazar las formas en el espacio virtual
  
-  var PieIzq = new THREE.Mesh(esferaForma);//pie izquierdo
-  var PieDer = new THREE.Mesh(esferaForma);//pie derecho
+  this.PieIzq = new THREE.Mesh(esferaForma);//pie izquierdo
+  this.PieDer = new THREE.Mesh(esferaForma);//pie derecho
+  this.PiernIzq = new THREE.Mesh(cilindroForma3);//pierna izq
+  this.PiernDer = new THREE.Mesh(cilindroForma3);//pierna derecha
+  
+ 
+ //se desplazan las mallas
+ 
+ this.PieIzq.position.x=-1;
+ this.PieIzq.position.y=-6;
+ 
+ this.PieDer.position.x=1;
+ this.PieDer.position.y=-6;
+ 
+ this.PiernIzq.position.x=-1;
+ this.PiernIzq.position.y=-4;
+ 
+ this.PiernDer.position.x=1;
+ this.PiernDer.position.y=-4;
+ 
+ this.add(this.PieIzq);
+ this.add(this.PiernIzq);
+ this.add(this.PieDer);
+ this.add(this.PiernDer);
+}
+Robotina.prototype =new THREE.Object3D();
+function setup(){
   var ManIzq = new THREE.Mesh(esferaForma);//mano isq
   var ManDer = new THREE.Mesh(esferaForma);//mano derecha
   var HombIzq = new THREE.Mesh(esferaForma);//hombro isq
@@ -19,20 +45,12 @@ var material= new THREE.MeshNormalMaterial();
   var Cabeza = new THREE.Mesh(esferaForma2);//cabeza
   var Torso = new THREE.Mesh(cilindroForma);//torso
   var Cadera = new THREE.Mesh(cilindroForma2);//cadera
-  var PiernIzq = new THREE.Mesh(cilindroForma3);//pierna izq
-  var PiernDer = new THREE.Mesh(cilindroForma3);//pierna derecha
+ 
   var BrazIzq = new THREE.Mesh(cilindroForma3);//brazo isq
   var BrazDer = new THREE.Mesh(cilindroForma3);//brazo derecho
   var Cuello = new THREE.Mesh(cilindroForma4);//cuello
- //se desplazan las mallas
- 
- PieIzq.position.x=-1;
- PieIzq.position.y=-6;
- 
- PieDer.position.x=1;
- PieDer.position.y=-6;
- 
- ManIzq.position.x=-2.25;
+  
+   ManIzq.position.x=-2.25;
  ManIzq.position.y=-0.6;
  
  ManDer.position.x=2.25;
@@ -53,11 +71,6 @@ var material= new THREE.MeshNormalMaterial();
  Cadera.position.x=0;
  Cadera.position.y=-1;
  
- PiernIzq.position.x=-1;
- PiernIzq.position.y=-4;
- 
- PiernDer.position.x=1;
- PiernDer.position.y=-4;
  
  BrazIzq.position.x=-2.25;
  BrazIzq.position.y=1.25;
@@ -68,33 +81,54 @@ var material= new THREE.MeshNormalMaterial();
  Cuello.position.x=0;
  Cuello.position.y=4.375;
  
+ PiernaD= new Robotina();
+ PiernaI=new Robotina();
+ PiernaD.position.z=-1;
+ PiernaI.position.z=1;
+ 
+ step=0.01;
+}
+
+ 
  //se genera una forma geometria abstracta
-  var forma = new THREE.Geometry();
+//  var forma = new THREE.Geometry();
  
  // se utiliza el paqyuete GeometryUtils para conjuntar las formas
- THREE.GeometryUtils.merge(forma, PieIzq);
- THREE.GeometryUtils.merge(forma, PieDer);
- THREE.GeometryUtils.merge(forma, ManIzq);
- THREE.GeometryUtils.merge(forma, ManDer);
- THREE.GeometryUtils.merge(forma, HombIzq);
- THREE.GeometryUtils.merge(forma, HombDer);
- THREE.GeometryUtils.merge(forma, Cabeza);
- THREE.GeometryUtils.merge(forma, Torso);
- THREE.GeometryUtils.merge(forma, Cadera);
- THREE.GeometryUtils.merge(forma, PiernIzq);
- THREE.GeometryUtils.merge(forma, PiernDer);
- THREE.GeometryUtils.merge(forma, BrazIzq);
- THREE.GeometryUtils.merge(forma, BrazDer);
- THREE.GeometryUtils.merge(forma, Cuello);
+// THREE.GeometryUtils.merge(forma, PieIzq);
+ //THREE.GeometryUtils.merge(forma, PieDer);
+ //THREE.GeometryUtils.merge(forma, ManIzq);
+ //THREE.GeometryUtils.merge(forma, ManDer);
+ //THREE.GeometryUtils.merge(forma, HombIzq);
+ //THREE.GeometryUtils.merge(forma, HombDer);
+ //THREE.GeometryUtils.merge(forma, Cabeza);
+ //THREE.GeometryUtils.merge(forma, Torso);
+ //THREE.GeometryUtils.merge(forma, Cadera);
+ //THREE.GeometryUtils.merge(forma, PiernIzq);
+ //THREE.GeometryUtils.merge(forma, PiernDer);
+ //THREE.GeometryUtils.merge(forma, BrazIzq);
+ //THREE.GeometryUtils.merge(forma, BrazDer);
+ //THREE.GeometryUtils.merge(forma, Cuello);
  
  //se genera la malla a partir de la forma 
  
- malla= new THREE.Mesh(forma, material);
+ //malla= new THREE.Mesh(forma, material);
  
  //Se inicializa la escena y se agrega la malla a esta 
  
  escena= new THREE.Scene();
  escena.add(malla);
+ escena.add(PiernaD);
+ escena.add(PiernaI);
+ escena.add(ManIzq);
+ escena.add(ManDer);
+ escena.add(HombIzq);
+ escena.add(HombDer);
+ escena.add(Cabeza);
+ escena.add(Torso);
+ escena.add(Cadera);
+ escena.add(BrazIzq);
+ escena.add(BrazDer);
+ escena.add(Cuello);
  //se inicializa la camara y el renderet
  
  camara= new THREE.PerspectiveCamera();
@@ -107,14 +141,20 @@ var material= new THREE.MeshNormalMaterial();
  
  function loop() {
   requestAnimationFrame(loop);
-  //Es importante notar que las rotaciones son sobre los ejes que  estan fijos a la malla, no los ejees del lienzo, inicialmente ambos coincide
-  
-  malla.rotation.x +=0.01;
-  malla.rotation.y +=0.01;
-  
   renderer.render(escena,camara);
+  //Es importante notar que las rotaciones son sobre los ejes que  estan fijos a la malla, no los ejees del lienzo, inicialmente ambos coincide
+  if(Math.abs(piernaD.rotation.z)>.5)
+    step = -step;
+    
+      piernaD.rotation.z+=step;
+      piernaI.rotation.z-=step;
+ // malla.rotation.x +=0.01;
+  //malla.rotation.y +=0.01;
+  
+ 
 }
 
-var escena, camara, renderer, malla;
+var escena, camara, renderer; //malla;
+var step, PiernaD, PiernaI;
 setup();
 loop();
