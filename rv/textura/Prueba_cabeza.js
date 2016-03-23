025f1063f2c239cdf40fdaceffda3379f8ba72ba
FerrerPonce <dianaferrerponce@gmail.com>
2016-03-22 21:18:44 -0600

    Update Prueba_cabeza.js

--- rv/textura/Prueba_cabeza.js
+++ rv/textura/Prueba_cabeza.js
@@ -3,7 +3,8 @@ function setup(){
  var textura=THREE.ImageUtils.loadTexture("cabeza.jpg");
  var material=new THREE.MeshBasicMaterial({map:textura});
  //var forma=new THREE.BoxGeometry(1,1,1);
- var forma=new THREE.SphereGeometry(10);
+// var forma=new THREE.SphereGeometry(10);
+ var forma= new THREE.CylinderGeometry(1,1,3.5);
  malla=new THREE.Mesh(forma,material);
  malla.rotation.z += 0.25;
 
