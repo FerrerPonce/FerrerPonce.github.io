5570fc28ea9fe3520dda0d2b311b0b8d0f50f106
FerrerPonce <dianaferrerponce@gmail.com>
2016-03-30 10:56:20 -0600

    Update Ejemplo2.js

--- rv/colisiones/Ejemplo2.js
+++ rv/colisiones/Ejemplo2.js
@@ -11,3 +11,20 @@ pelota=new THREE.Mesh(new THREE.SphereGeometry(0.5),
     camara= new THREE.Perspective.Camera();
     camara.position.z=20;
     
+     raycaster1 =new THREE.Raycaster( pelota.position, new THREE.vector3(1,0,0));
+    raycaster2 =new THREE.Raycaster( pelota.position, new THREE.vector3(-1,0,0));
+    
+    escena =new THREE.Scene();
+    escena.add (cubo1);
+    escena.add (cubo2);
+    escena.add (pelota);
+    escena.add (camara);
+    
+    renderer=new THREE.WebGLRenderer();
+    renderer.setSize(window.innerHeight*.95, 
+                    window.innerHeight*.95, );
+  
+  document.body.appendChild(renderer.domElement);
+  step=0.01;
+}
+
