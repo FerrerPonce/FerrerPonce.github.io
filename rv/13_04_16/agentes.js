711296eb41e3404ae5696f4f612f4f6ba1aef7e1
FerrerPonce <dianaferrerponce@gmail.com>
2016-04-13 11:09:31 -0500

    Create agentes.js

---
+++ rv/13_04_16/agentes.js
@@ -0,0 +1,36 @@
+function Agent (x=0, y=0){
+  TRHEE.Object.Call(this);
+  this.position.x=x;
+this.position.y=y;
+}
+
+Agent.prototype = new THREE.Object3D();
+ Agent.enviroment.sense() = function (enviroment{};
+  Agent.entviroment.plan()= function (enviroment{};
+  Agent.entviroment.act()= function (enviroment{};
+  
+  function Enviroment(){
+    THREE.Scene.call(this);
+    }
+    
+    Entviroment.prototype =new THREE Scene();
+    
+    Entviroment.prototype = new THREE.Scene;
+    
+    Enviroment.prototype.sence=function(){
+      for(var i=0; i< this.children.length; i++){
+      if(this.children[i].sense !==undefined)
+      this.children[1].sense(this);
+      }
+    
+    Enviroment.prototype.plan=function(){
+      for(var i=0; i< this.children.length; i++){
+      if(this.children[i].plan !==undefined)
+      this.children[1].plan(this);
+      }
+      
+    Enviroment.prototype.act=function(){
+      for(var i=0; i< this.children.length; i++){
+      if(this.children[i].act !==undefined)
+      this.children[1].act(this);
+      }
