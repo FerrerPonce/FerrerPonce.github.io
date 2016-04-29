4cc14ec366b909e5a2166649d07cbe39a950fc57
FerrerPonce <dianaferrerponce@gmail.com>
2016-04-29 13:43:23 -0500

    Update Lampara.js

--- rv/29_04_16/Lampara.js
+++ rv/29_04_16/Lampara.js
@@ -1,46 +1,40 @@
 function setup(){
-//se plantea las geometrias a utillixzar
+	malla= new THREE.Mesh(new THREE.CylinderGeometry(1,1,4,100),new THREE.MeshPhongMaterial({color: 0xff0000}));
+	malla.position.y +=2;
+	malla.position.z +=0.25;
 
+	var base = new THREE.Mesh(new THREE.BoxGeometry(5,.1,5),new THREE.MeshPhongMaterial({color: 0x00ff00}));
 
-var cilindroForma= new THREE.CylinderGeometry(1,1,4,100);
- //Para generar una forma combinada se requiere de las mallas poaras poder desplazar las formas en el espacio virtual
- 
-  var cilindro = new THREE.Mesh(cilindroForma);
- //se desplazan las mallas
- 
- var forma = new THREE.Geometry();
- // se utiliza el paqyuete GeometryUtils para conjuntar las formas
- 
- THREE.GeometryUtils.merge(forma, cilindro);
- 
- //se genera la malla a partir de la forma 
- 
- malla= new THREE.Mesh(forma);
- 
- //Se inicializa la escena y se agrega la malla a esta 
- 
- escena= new THREE.Scene();
- escena.add(malla);
- //se inicializa la camara y el renderet
- 
- camara= new THREE.PerspectiveCamera();
- camara.position.z=10;
- 
- renderer= new THREE.WebGLRenderer();
- renderer.setSize(window.innerHeight*.95, window.innerHeight*.95);
- document.body.appendChild(renderer.domElement);
- }
- 
- function loop() {
-  requestAnimationFrame(loop);
-  //Es importante notar que las rotaciones son sobre los ejes que  estan fijos a la malla, no los ejees del lienzo, inicialmente ambos coincide
-  
-  //malla.rotation.x +=0.01;
-  malla.rotation.y +=0.01;
-  
-  renderer.render(escena,camara);
+	var iluminacion = new THREE.PointLight(0xffffff);
+	iluminacion.position.x=20;
+
+	escena= new THREE.Scene();
+	escena.add(malla);
+	escena.add(base);
+	escena.add(iluminacion);
+
+	camara= new THREE.PerspectiveCamera();
+	camara.position.z=15;
+	camara.position.y=5;
+
+	renderer= new THREE.WebGLRenderer();
+	renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
+	document.body.appendChild(renderer.domElement);
+	
+	renderer.shadowMapEnabled = true;
+	malla.castShadow = true;
+	base.receiveShadow = true;
+	iluminacion.castShadow = true;
 }
 
-var escena, camara, renderer, malla;
+function loop(){
+	requestAnimationFrame(loop);
+
+	malla.rotation.y += 0.01;
+
+	renderer.render(escena,camara);
+}
+
+var camara, escena, renderer, malla;
 setup();
 loop();
