78a8758c5edc72d15936086ba6711fb7b4e44281
FerrerPonce <dianaferrerponce@gmail.com>
2016-04-29 14:17:23 -0500

    Update Lampara.js

--- rv/29_04_16/Lampara.js
+++ rv/29_04_16/Lampara.js
@@ -1,6 +1,6 @@
 function setup(){
 	malla= new THREE.Mesh(new THREE.CylinderGeometry(1,1,4,100),new THREE.MeshNormalMaterial({color: 0xff0000}));
-	malla.opacity=0.5;
+	malla.opacity=0.9;
 	malla.position.y +=3;
 	malla.position.z +=0.25;
 
