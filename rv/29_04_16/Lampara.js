4af7b2bf7fdc746e2d0b5e8aaead6bcd1594fed0
FerrerPonce <dianaferrerponce@gmail.com>
2016-04-29 13:31:48 -0500

    Update Lampara.js

--- rv/29_04_16/Lampara.js
+++ rv/29_04_16/Lampara.js
@@ -1,25 +1,16 @@
 function setup(){
 //se plantea las geometrias a utillixzar
 
-var esferaForma= new THREE.SphereGeometry(1);
+
 var cilindroForma= new THREE.CylinderGeometry(0.5,0.5,4);
  //Para generar una forma combinada se requiere de las mallas poaras poder desplazar las formas en el espacio virtual
  
-  var esfera1 = new THREE.Mesh(esferaForma);
-  var esfera2 = new THREE.Mesh(esferaForma);
   var cilindro = new THREE.Mesh(cilindroForma);
  //se desplazan las mallas
- 
- esfera1.position.y=2;
- esfera2.position.y=-2;
- 
- //se genera una forma geometria abstracta
  
  var forma = new THREE.Geometry();
  // se utiliza el paqyuete GeometryUtils para conjuntar las formas
  
- THREE.GeometryUtils.merge(forma, esfera1);
- THREE.GeometryUtils.merge(forma, esfera2);
  THREE.GeometryUtils.merge(forma, cilindro);
  
  //se genera la malla a partir de la forma 
